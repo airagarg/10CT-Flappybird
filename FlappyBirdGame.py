302d440bd8ccdae3a654f2fc68db15e9802e2a1a
DunneTeaches <54336909+DunneTeaches@users.noreply.github.com>
2026-03-24 10:47:50 +1100

    Initial commit

---
+++ FlappyBirdGame.py
@@ -0,0 +1,29 @@
+import pygame
+from pygame.locals import *
+import random
+
+# Initiates PyGame module
+pygame.init()
+
+
+# Set screen dimensions
+screen_width = 800
+screen_height = 900
+
+# Creates the game screen
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('Flappy Bird')
+
+
+# Main game Loop
+run = True
+while run:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+        
+    pygame.display.update()
+
+# Quits PyGame
+pygame.quit()
